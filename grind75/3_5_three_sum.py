3ae6ac46344e55c77f4f10aa4ae762c4026e2eda
Jeffrey Chuc <jeffreychuc@gmail.com>
2024-04-18 16:42:45 -0700

    add 3sum

---
+++ grind75/3_5_three_sum.py
@@ -0,0 +1,67 @@
+# 15. 3Sum
+# Solved
+# Medium
+# Topics
+# Companies
+# Hint
+# Given an integer array nums, return all the triplets [nums[i], nums[j], nums[k]]
+# such that i != j, i != k, and j != k, and nums[i] + nums[j] + nums[k] == 0.
+#
+# Notice that the solution set must not contain duplicate triplets.
+#
+#
+#
+# Example 1:
+#
+# Input: nums = [-1,0,1,2,-1,-4]
+# Output: [[-1,-1,2],[-1,0,1]]
+# Explanation:
+# nums[0] + nums[1] + nums[2] = (-1) + 0 + 1 = 0.
+# nums[1] + nums[2] + nums[4] = 0 + 1 + (-1) = 0.
+# nums[0] + nums[3] + nums[4] = (-1) + 2 + (-1) = 0.
+# The distinct triplets are [-1,0,1] and [-1,-1,2].
+# Notice that the order of the output and the order of the triplets does not matter.
+# Example 2:
+#
+# Input: nums = [0,1,1]
+# Output: []
+# Explanation: The only possible triplet does not sum up to 0.
+# Example 3:
+#
+# Input: nums = [0,0,0]
+# Output: [[0,0,0]]
+# Explanation: The only possible triplet sums up to 0.
+#
+#
+# Constraints:
+#
+# 3 <= nums.length <= 3000
+# -105 <= nums[i] <= 105
+
+from typing import List
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()  # [-4, -1, -1, 0, 1, 2]
+        res = set()
+
+        for i in range(len(nums) - 1):  # we want -1 here because we're going to start the right pointer at the end
+            if i > 0 and nums[i] == nums[i - 1]:
+                # we want to skip repeats that we've already tried,
+                # because this list is sorted its going to be contiguous
+                # this is an optimization needed for really large lists
+                continue
+
+            l = i + 1
+            r = len(nums) - 1
+            while l < r:
+                if nums[i] + nums[l] + nums[r] == 0:
+                    res.add((nums[i], nums[l], nums[r]))
+                    l += 1
+                elif nums[i] + nums[l] + nums[r] < 0:
+                    l += 1
+                elif nums[i] + nums[l] + nums[r] > 0:
+                    r -= 1
+
+        return [[x, y, z] for x, y, z in res]
